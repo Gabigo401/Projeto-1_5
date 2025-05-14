27aa31cbf4e41f4dfff5565440700605155ef2e3
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-13 22:50:10 -0300

    Fazendo um typedef para enumeração das possiveis respostar que o sistema vai mostrar ao fazer alguma ação

--- tarefas.h
+++ tarefas.h
@@ -1,1 +1,12 @@
 #include <stdio.h>
+
+typedef enum {  //enumeração para as possiveis respostas do sistema
+    OK,   //operação feita com sucesso
+    MAX_TAREFA,  //limite atingido de tarefas
+    SEM_TAREFAS,   //não tem tarefas para exibir
+    NAO_ENCONTRADO,   //a tarefa não foi encontrada
+    ABRIR,  //abrindo arquivo
+    FECHAR,  //fechando arquivo
+    ESCREVER,   //escrevendo no arquivo
+    LER   //lendo o arquivo
+  } Status;   //nome desse typedef
