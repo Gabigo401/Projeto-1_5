6b1d57300a08fcdc2817b1f5e8509b63201a6c4c
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-13 23:34:53 -0300

    Criando a função de listar e um array para a variavel listar e um ponteiro para o indice e uma verificação se tem alguma tarefa ja criada e se não tiver retorna a mensagem sem_tarefas

--- tarefas.c
+++ tarefas.c
@@ -1,3 +1,7 @@
 #include "tarefas.h"
 #include <stdio.h>
 #include <string.h>
+
+Status listar(Tarefa tarefas[], int *pos) { //criando a função de listar as tarefas e um array de tarefas e um ponteiro int para indice
+    if (*pos == 0)   //verifica se tem tarefas criadas
+      return SEM_TAREFAS;   //se não tem volta essa mensagem
