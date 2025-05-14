7b65626ac9b31b8d1214d03c93e6786a8725a2e7
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-14 00:04:04 -0300

    função para limpar o buffer sempre apos um scanf limpanddo todo caractere que sobra

--- tarefas.c
+++ tarefas.c
@@ -1,3 +1,9 @@
 #include "tarefas.h"
 #include <stdio.h>
 #include <string.h>
+
+void clearBuffer() {  //void para limpar o buffer
+    int c;   //int chamado c 
+    while ((c = getchar()) != '\n' && c != EOF)   //limpa o buffer do teclado apos um scanf
+      ;
+  }
