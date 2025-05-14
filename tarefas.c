c13d8c9a325d20848454a4942f6e0a9671bc0805
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-13 23:19:38 -0300

    criando a função de deletar e um array de tarefas alem de ponteiro int para o inice e depois verifica se teme alguma tarefa para deletar e se não tiver volta uma mensagem  sem tarefas

--- tarefas.c
+++ tarefas.c
@@ -1,3 +1,7 @@
 #include "tarefas.h"
 #include <stdio.h>
 #include <string.h>
+
+Status deletar(Tarefa tarefas[], int *pos) { //criando função de deletar e um array para tarefas alem de um ponteiro de int para o indice
+    if (*pos == 0)  //verifica se existe alguma tarefa no sistema
+      return SEM_TAREFAS;  //se não tem nenhuma tarefa retorna a mensagem sem_tarefa
