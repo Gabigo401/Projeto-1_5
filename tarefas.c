cdc5dfed2714b07f4e6d686532e92b5a44e77ac6
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-13 23:56:43 -0300

    função para ler e um array de tarefas e ponteiro int para o indice e ddepois abre o arquivo para ler e verifica se ele foi aberto

--- tarefas.c
+++ tarefas.c
@@ -1,3 +1,8 @@
 #include "tarefas.h"
 #include <stdio.h>
 #include <string.h>
+
+Status carregar(Tarefa tarefas[], int *pos) {  //função para ler e um array de tarefas e ponteiro int para o indice
+    FILE *f = fopen("tarefas.bin", "rb");  //abre o arquivo para leitura
+    if (f == NULL)   //verifica se o arquivo foi aberto
+      return ABRIR;   //se não foi aberto aparece essa mensagem de erro
