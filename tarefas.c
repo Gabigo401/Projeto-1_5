394aafd420da72fd770620ddb850f83893726aab
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-13 23:38:15 -0300

    cria uma variavel i para percorrer a array de tarefas e mostrar os detalher de cada uma e uma mensagem pos para mostrar a posição e se no final a ação ocorreu sem problemas aparece a mensagem ok como sucesso

--- tarefas.c
+++ tarefas.c
@@ -5,3 +5,16 @@
 Status listar(Tarefa tarefas[], int *pos) { //criando a função de listar as tarefas e um array de tarefas e um ponteiro int para indice
     if (*pos == 0)   //verifica se tem tarefas criadas
       return SEM_TAREFAS;   //se não tem volta essa mensagem
+
+
+    int i;
+    for (i = 0; i < *pos; i++) {  //usa a variavel i para percorrer o array tarefas e mostrar os detalher de cada
+        printf("Pos: %d\t", i + 1);   //mostra a posição
+        printf("Prioridade: %d\t", tarefas[i].prioridade);
+        printf("Categoria: %s\t", tarefas[i].categoria);
+        printf("Descricao: %s\n", tarefas[i].descricao);
+    }
+    
+    return OK;  //retorna essa mensagem se a ação for um sucesso
+}
+    
