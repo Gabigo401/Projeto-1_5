8015f3d1adae349dff7fe83ba98e843f6720245f
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-14 00:01:20 -0300

    int que le todas as informações das tarefas como categoria e descrição e verifica se foi aberto o arquivo e lido, le a posição da lista e verifica se foi lido e depois fechao arquivo

--- tarefas.c
+++ tarefas.c
@@ -6,3 +6,18 @@ Status carregar(Tarefa tarefas[], int *pos) {  //função para ler e um array de
     FILE *f = fopen("tarefas.bin", "rb");  //abre o arquivo para leitura
     if (f == NULL)   //verifica se o arquivo foi aberto
       return ABRIR;   //se não foi aberto aparece essa mensagem de erro
+
+    int qtd = fread(tarefas, sizeof(Tarefa), TOTAL, f);   //le todas as informações dar tarefas como categoria e descrição
+    if (qtd == 0)   //verifica se foi aberto e lido
+        return LER;
+    
+    qtd = fread(pos, sizeof(int), 1, f);   //le a posição da lista
+    if (qtd == 0)   //verifica se foi aberto e lido
+        return LER;
+    
+    if (fclose(f))  //fecha o arquivo
+        return FECHAR;
+    
+    return OK;  //mensagem de sucesso
+};
+    
