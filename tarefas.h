2ce9624290e24dcf0c4775aa987c526278b01b82
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-13 22:55:36 -0300

    Declarando as funções que serão ddeclaradas em outro arquivo alem de função de limpar o buffer

--- tarefas.h
+++ tarefas.h
@@ -1,1 +1,11 @@
 #include <stdio.h>
+
+typedef Status (*funcao)(Tarefa[], int *);   //receber um array para tarefas e um ponteiro de int que vai ser usaddo para indice
+
+Status criar(Tarefa tarefas[], int *pos);   //declarando a função criar
+Status deletar(Tarefa tarefas[], int *pos);  //declarando a função deletar
+Status listar(Tarefa tarefas[], int *pos);  //declarando a função listar
+Status salvar(Tarefa tarefas[], int *pos);  //declarando a função salvar
+Status carregar(Tarefa tarefas[], int *pos);  //declarando a função carregar
+
+void clearBuffer();  //função para limpar o buffer
