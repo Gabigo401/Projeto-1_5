3ea1918d7b87ad4344926e887413fb75572803b9
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-13 23:26:27 -0300

    criando um int que recebe a resposta em seu endereço da pergunta de posição desejada da tarefa para deletar e decrementa esse valor, apos isso, um if para verificar se a posição e valida usando a posição base de 0 e se não foi valida retorna a mensagem de não encontrao

--- tarefas.c
+++ tarefas.c
@@ -5,3 +5,12 @@
 Status deletar(Tarefa tarefas[], int *pos) { //criando função de deletar e um array para tarefas alem de um ponteiro de int para o indice
     if (*pos == 0)  //verifica se existe alguma tarefa no sistema
       return SEM_TAREFAS;  //se não tem nenhuma tarefa retorna a mensagem sem_tarefa
+
+    int pos_deletar;   //variavel int
+    printf("Entre com a posicao da tarefa a ser deletada: ");  //pedindo a posição da lista que sera deletada
+    scanf("%d", &pos_deletar);  //le a resposta e guarda no endereço de pos_deletar
+    clearBuffer();  //limpa o buffer apos o scanf
+    
+    pos_deletar--;  //decrementa a variavel
+    if (pos_deletar >= *pos || pos_deletar < 0) //if para verificar se a posição e valida ajustando a posição de base o 0
+    return NAO_ENCONTRADO;  //se não for volta essa mensagem
