fdca693d3d76f7799b425836ff8da11c84412d50
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-13 23:49:11 -0300

    Criando a função de listar e um array de tarefas e ponteiro para o indice, apos isso, abre o arquivo e verificar se ele foi aberto

--- tarefas.c
+++ tarefas.c
@@ -1,3 +1,8 @@
 #include "tarefas.h"
 #include <stdio.h>
 #include <string.h>
+
+Status salvar(Tarefa tarefas[], int *pos) {  //função para salvar e um array de tarefas e um ponteiro para o indice
+    FILE *f = fopen("tarefas.bin", "wb");  //abre um arquivo binario para escrever nele
+    if (f == NULL)   //verifica se o arquivo binario foi aberto
+      return ABRIR;  //mensagem se não abriu
