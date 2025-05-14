f592e5160831f7899621673ca0bfa34eb7e7f133
Gabigo401 <gabrielchoque017@gmail.com>
2025-05-13 23:06:07 -0300

    Criando a função de  criar tarefa que recebe um array de tarefa e um ponteiro int para os indices e depois verifica se ja chegou ao maximo de tarefas e se sim retornar como MAX_TAREFAS

--- tarefas.c
+++ tarefas.c
@@ -1,3 +1,7 @@
 #include "tarefas.h"
 #include <stdio.h>
 #include <string.h>
+
+Status criar(Tarefa tarefas[], int *pos) {  //a função criar que recebe um array de tarefas e um ponteiro int para os indices
+    if (*pos >= TOTAL)   //verificar se o numero de tarefas atingiu o limite
+      return MAX_TAREFA;   //se atingiu retorna como MAX_TAREFA
